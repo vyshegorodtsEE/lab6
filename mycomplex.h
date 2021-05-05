c85f87edc1b1cfe5c00bd1f55091ef441e14af8c
Elizaveta Vyshegorodtseva <ereshtnikova@mail.ru>
2021-05-05 12:54:58 +0300

    thurd commit

--- mycomplex.h
+++ mycomplex.h
@@ -8,31 +8,31 @@ class Complex
 {
     double Re;
     double Im;
-    public:
 
+    public:
         Complex(double aRe = 0, double aIm = 0);
         Complex(const Complex &);
         ~ Complex();
 
-void Set(double aRe, double aIm = 0);
-     operator double();
-     double abs();
+    void Set(double aRe, double aIm = 0);
+        operator double();
+        double abs();
 
-friend istream & operator >> (istream &, Complex &);
-friend ostream & operator << (ostream &, Complex &);
+    friend istream & operator >> (istream &, Complex &);
+    friend ostream & operator << (ostream &, Complex &);
 
         Complex operator + (const Complex &);
         Complex operator - (const Complex &);
         Complex operator + (const double &);
 
-friend Complex operator + (const double &, const Complex &);
-	Complex operator - (const double &);
-        
-friend Complex operator - (const double &, const Complex &);
-	Complex operator * (const Complex &);
-        Complex operator * (const double&);
-        
-friend Complex operator * (const double &, const Complex &);
+    friend Complex operator + (const double &, const Complex &);
+        Complex operator - (const double &);
+
+    friend Complex operator - (const double &, const Complex &);
+        Complex operator * (const Complex &);
+        Complex operator * (const double &);
+
+    friend Complex operator * (const double &, const Complex &);
         Complex operator / (const double &);
         Complex & operator += (const Complex &);
         Complex & operator -= (const Complex &);
